053cfc15ade28ecddf78af090ca15ea8aceb31c9
dev-soks <victoryasokomeh@josla.com.ng>
2020-07-24 15:36:31 +0100

    prepare for deploy

---
+++ client/build/precache-manifest.6627acf682a89e08f5c3b118c1642a26.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "35d558f1e89ca8be438dcf837fcc9ac7",
+    "url": "/index.html"
+  },
+  {
+    "revision": "f22e94ff43556f6a6dd3",
+    "url": "/static/css/2.4819d237.chunk.css"
+  },
+  {
+    "revision": "18c2c5c877adb7157bdf",
+    "url": "/static/css/main.78c03da1.chunk.css"
+  },
+  {
+    "revision": "f22e94ff43556f6a6dd3",
+    "url": "/static/js/2.b12995b4.chunk.js"
+  },
+  {
+    "revision": "cacaf3f01dd3bd8237006f7c58951c1c",
+    "url": "/static/js/2.b12995b4.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "18c2c5c877adb7157bdf",
+    "url": "/static/js/main.6ad77ce2.chunk.js"
+  },
+  {
+    "revision": "702554416a81a1e3a894",
+    "url": "/static/js/runtime-main.784c586e.js"
+  }
+]);
